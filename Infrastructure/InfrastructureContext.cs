2b63ed516c4b1c5f9a408b17cfff7b2215f257f0
rmona <rmona@5CD04958LJ>
2022-04-25 00:10:33 +0300

    add user migration

---
+++ Infrastructure/InfrastructureContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class InfrastructureContext : IdentityDbContext<AppUser>
+    {
+
+        public InfrastructureContext(DbContextOptions<InfrastructureContext> options) : base(options)
+        {
+        }
+    }
+}
